a9529a4622634dbdb6a065f169f203e00d50eb6b
cuongduong2710 <cuongduongngocquoc@gmail.com>
2024-09-25 09:40:24 +0700

    connect db Mongo

--- next-events/app/page.js
+++ next-events/app/page.js
@@ -1,14 +1,24 @@
-import Button from "./component/Button"
+import { dbConnect } from "./lib/dbConnect"
 
 export const metadata = {
   title: 'Home'
 }
 
 export default async function Home() {
+  let connection;
+  try {
+    connection = await dbConnect()
+  } catch (error) {
+    console.log(error)
+  }
   
   return  (
     <div className='m-2'>
-      <Button />
+      { connection ? (
+        <h1>Database is connected!</h1>
+      ): (
+        <h1>Database is NOT connected!</h1>
+      )}
     </div>
   )
 }
