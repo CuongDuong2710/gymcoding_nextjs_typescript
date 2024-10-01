4cc812caa93d7688e8216888487032eb89802a37
cuongduong2710 <cuongduongngocquoc@gmail.com>
2024-10-01 09:52:52 +0700

    nested object types

--- typescript/custom_types.ts
+++ typescript/custom_types.ts
@@ -5,16 +5,33 @@ type Vehicle = {
     model: string
     year: number
     isElectric: boolean
+    engine: Engine
+}
+
+type Engine = {
+    type: string
+    horsepower: number
+    fuelType: string
 }
 
 let vehicle: Vehicle = {
     model: 'Civic',
     year: 2021,
-    isElectric: false
+    isElectric: false,
+    engine: {
+        type: 'Inline-4',
+        horsepower: 158,
+        fuelType: 'Gasoline'
+    }
 }
 
 let vehicle2: Vehicle = {
     model: 'Model3',
     year: 2023,
-    isElectric: true
+    isElectric: true,
+    engine: {
+        type: 'Inline-4',
+        horsepower: 158,
+        fuelType: 'Gasoline'
+    }
 }
