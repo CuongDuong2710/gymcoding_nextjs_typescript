9b297554ce0ac4ec5b591016ecafdc010004f6ec
cuongduong2710 <cuongduongngocquoc@gmail.com>
2024-10-04 09:33:53 +0700

    use Any type and assign function to update car

--- typescript/custom_types.js
+++ typescript/custom_types.js
@@ -1,10 +1,18 @@
-"use strict";
-let primaryInstrument = 'guitar';
+var primaryInstrument = 'guitar';
 function displayVehicleInfo(vehicle) {
     var _a;
-    console.log(`${vehicle.model} has a ${(_a = vehicle.engine) === null || _a === void 0 ? void 0 : _a.type} engine.`); // 'vehicle.engine' is possibly 'undefined'
+    console.log("".concat(vehicle.model, " has a ").concat((_a = vehicle.engine) === null || _a === void 0 ? void 0 : _a.type, " engine.")); // 'vehicle.engine' is possibly 'undefined'
 }
-let vehicle = {
+function updateVehicle(id, updates) {
+    var foundVehicle = vehicles.find(function (vehicle) { return vehicle.id === id; });
+    if (!foundVehicle) {
+        console.error('Vehicle not found');
+        return;
+    }
+    Object.assign(foundVehicle, updates);
+}
+var vehicle = {
+    id: 1,
     model: 'Civic',
     year: 2021,
     isElectric: false,
@@ -12,14 +20,34 @@ let vehicle = {
         type: 'Inline-4',
         horsepower: 158,
         fuelType: 'Gasoline'
-    }
+    },
+    vehicleType: 'car'
 };
-let vehicle2 = {
+var vehicle2 = {
+    id: 2,
     model: 'Model3',
     year: 2023,
-    isElectric: true
+    isElectric: true,
+    vehicleType: 'car'
 };
-displayVehicleInfo(vehicle); // Civic has a Inline-4 engine.
-displayVehicleInfo(vehicle2); // Model3 has a undefined engine.
+var vehicle3 = {
+    id: 3,
+    model: 'Ford F-150',
+    year: 2024,
+    isElectric: false,
+    vehicleType: 'truck'
+};
+var vehicles = [
+    vehicle,
+    vehicle2,
+    vehicle3
+];
+// displayVehicleInfo(vehicle) // Civic has a Inline-4 engine.
+// displayVehicleInfo(vehicle2) // Model3 has a undefined engine.
 // tsc custom_types.ts -> compile into custom_types.js
 // node custom_types.js
+updateVehicle(1, { model: 'Honda Accord' });
+updateVehicle(3, {
+    vehicleType: 'car'
+});
+console.log(vehicles);
--- typescript/custom_types.ts
+++ typescript/custom_types.ts
@@ -6,6 +6,7 @@ type VehicleType = 'motocycle' | 'car' | 'truck'
 // let vehicleType: VehicleType = 'abc' // Type '"abc"' is not assignable to type 'VehicleType'
 
 type Vehicle = {
+    id: number
     model: string
     year: number
     isElectric: boolean
@@ -23,7 +24,19 @@ function displayVehicleInfo(vehicle: Vehicle) {
     console.log(`${vehicle.model} has a ${vehicle.engine?.type} engine.`) // 'vehicle.engine' is possibly 'undefined'
 }
 
+function updateVehicle(id: number, updates: any) { // `updates` is an object that only contain the properties we want to change
+    const foundVehicle = vehicles.find(vehicle => vehicle.id === id)
+
+    if (!foundVehicle) {
+        console.error('Vehicle not found')
+        return
+    }
+
+    Object.assign(foundVehicle, updates)
+}
+
 let vehicle: Vehicle = {
+    id: 1,
     model: 'Civic',
     year: 2021,
     isElectric: false,
@@ -36,14 +49,34 @@ let vehicle: Vehicle = {
 }
 
 let vehicle2: Vehicle = {
+    id: 2,
     model: 'Model3',
     year: 2023,
     isElectric: true,
     vehicleType: 'car'
 }
 
-displayVehicleInfo(vehicle) // Civic has a Inline-4 engine.
-displayVehicleInfo(vehicle2) // Model3 has a undefined engine.
+let vehicle3: Vehicle = {
+    id: 3,
+    model: 'Ford F-150',
+    year: 2024,
+    isElectric: false,
+    vehicleType: 'truck'
+}
+
+const vehicles: Vehicle[] = [
+    vehicle,
+    vehicle2,
+    vehicle3
+]
+
+// displayVehicleInfo(vehicle) // Civic has a Inline-4 engine.
+// displayVehicleInfo(vehicle2) // Model3 has a undefined engine.
 
 // tsc custom_types.ts -> compile into custom_types.js
 // node custom_types.js
+
+updateVehicle(1, { model: 'Honda Accord' })
+updateVehicle(3, { 
+    vehicleType: 'car' })
+console.log(vehicles)
